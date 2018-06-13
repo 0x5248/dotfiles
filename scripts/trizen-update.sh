6ee21b52c66a735405d958e4b3919bc596ad0ec4
0x5248 <robert.herr95@hotmail.com>
2018-06-13 16:43:44 +0200

    Added explaining comments in the trizen-update.sh how\n to update the repos without typing in the sudo password

--- scripts/trizen-update.sh
+++ scripts/trizen-update.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# to run this command without typing in the sudo password
+# add 'USERNAME HOSTNAME=NOPASSWD: /bin/pacman -Sy' at 
+# the end of the file /etc/sudoers
 trizen -Sy
 
 UPDATE="$(trizen -Qu | wc -l)"
